b4e868e58501cbca51b8d22c78ae2569095712f2
anderswag <anderson.hung@live.com>
2017-01-11 13:18:25 -0500

    styled fonts and background and implemented links to cards

--- App.jsx
+++ App.jsx
@@ -3,36 +3,53 @@ import * as Data from './torontoData.js';
 
 let today = Date.now()
 
+let formatDate = (rawDate) => {return rawDate.slice(0,10)}
+
+let dateFilter = (date) => {
+  if(Date.parse(date) > today) {
+    return true
+  }
+}
+
 class Card extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       title: this.props.title,
       date: this.props.date,
-      style: this.props.style
+      style: this.props.style,
+      url: this.props.url
     }
   }
 
 
   render() {
     return (
-      <div className="col-4">
-        <div style={this.state.style} className="card">
-          <div className="card-info">
-            <h5>{this.state.title}</h5>
-            <p>{this.state.date}</p>
+      <a href={this.state.url}>
+        <div className="col-4">
+          <div style={this.state.style} className="card">
+            <div className="card-info">
+              <h5>{this.state.title}</h5>
+              <p>{this.state.date}</p>
+            </div>
           </div>
         </div>
-      </div>
+      </a>
     )
   }
 }
 
-let formatDate = (rawDate) => {return rawDate.slice(0,10)}
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-let dateFilter = (date) => {
-  if(Date.parse(date) > today) {
-    return true
+  render() {
+    return (
+      <nav className="nav-bar">
+        <p>doToronto</p>
+      </nav>
+    )
   }
 }
 
@@ -45,24 +62,28 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="container">
-        <div className="row">
-        {this.state.activities.default.data.map(function(activity,index){
-          if(activity.calEvent.thumbImage && dateFilter(activity.calEvent.startDate)){
-            return (
-              <Card
-                title={activity.calEvent.eventName}
-                key={index}
-                date={formatDate(activity.calEvent.startDate)}
-                style={{
-                  'backgroundImage':`url('http://app.toronto.ca/${activity.calEvent.thumbImage.url}')`,
-                  'backgroundRepeat':'no-repeat',
-                  'backgroundSize':'cover'
-                }}
-              />
-            )
-          }
-        })}
+      <div>
+        <Navbar />
+        <div className="container">
+          <div className="row">
+          {this.state.activities.default.data.map(function(activity,index){
+            if(activity.calEvent.thumbImage && dateFilter(activity.calEvent.startDate)){
+              return (
+                <Card
+                  title={activity.calEvent.eventName}
+                  key={index}
+                  date={formatDate(activity.calEvent.startDate)}
+                  style={{
+                    'backgroundImage':`url('http://app.toronto.ca/${activity.calEvent.thumbImage.url}')`,
+                    'backgroundRepeat':'no-repeat',
+                    'backgroundSize':'cover'
+                  }}
+                  url={activity.calEvent.eventWebsite}
+                />
+              )
+            }
+          })}
+          </div>
         </div>
       </div>
     );
